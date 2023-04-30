0094dd234819722605dd2687cf941c599ad88457
rmaxbaer <rmaxbaer@gmail.com>
2023-04-30 17:26:15 -0600

    commentsss

--- lambda_function.py
+++ lambda_function.py
@@ -1,8 +1,6 @@
 import json
-from secondfile import brookeiscute
 
 def lambda_handler(event, context):
     print('its chewsday, init bruv. bloody good day for a pint' )
-    #brookeiscute(event)
     # lambda_function = 2
     # lambda_function = 3
