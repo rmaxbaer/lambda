6e795640ea75f2444c032902ef7a98f13aae57e5
rmaxbaer <rmaxbaer@gmail.com>
2023-04-30 17:24:23 -0600

    comments

--- lambda_function.py
+++ lambda_function.py
@@ -3,6 +3,6 @@ from secondfile import brookeiscute
 
 def lambda_handler(event, context):
     print('its chewsday, init bruv. bloody good day for a pint' )
-    brookeiscute(event)
-    lambda_function = 2
-    lambda_function = 3
+    #brookeiscute(event)
+    # lambda_function = 2
+    # lambda_function = 3
