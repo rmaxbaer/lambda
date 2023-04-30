2e936a7052885c1864cb5635b5deb744e5ba6342
rmaxbaer <rmaxbaer@gmail.com>
2023-04-30 15:40:29 -0600

    comment after chewsday

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ import json
 from secondfile import brookeiscute
 
 def lambda_handler(event, context):
-    print('its chewsday init bruv')
+    print('its chewsday, init bruv')
     brookeiscute(event)
