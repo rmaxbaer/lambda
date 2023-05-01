af8d73ed0cb211c0c5ec55ecd4a8568dbeb0e922
rmaxbaer <rmaxbaer@gmail.com>
2023-04-30 18:33:13 -0600

    4

--- lambda_function.py
+++ lambda_function.py
@@ -1,6 +1,6 @@
 import json
 
 def lambda_handler(event, context):
-    print('its chewsday. bloody good day for a pint, init bruv 4' )
+    print('its chewsday. bloody good day for a pint, init bruv 5' )
     # lambda_function = 2
     # lambda_function = 3
